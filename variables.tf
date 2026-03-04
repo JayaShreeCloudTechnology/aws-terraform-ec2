a5f1022d4759a230f00d8b20220eaf488c602158
JayaShreeCloudTechnology <jayashreecloudtechnology@gmail.com>
2026-03-04 09:50:13 +0530

    adding aws infra

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "ami_id" {
+  default = "ami-051a31ab2f4d498f5" # Amazon Linux 3 (ap-south-1)
+}
