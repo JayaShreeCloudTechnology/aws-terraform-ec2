97801b88a6611fd48f4cb9444e42c77148f01cf6
JayaShreeCloudTechnology <jayashreecloudtechnology@gmail.com>
2026-03-04 13:29:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,6 @@ resource "aws_instance" "ec2_instance" {
     Name = "Test_Server"
   }
 }
-<<<<<<< HEAD
-
 # Define the S3 bucket resource
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-unique-bucket-name-hyd2026" # Bucket names must be globally unique across all of AWS
